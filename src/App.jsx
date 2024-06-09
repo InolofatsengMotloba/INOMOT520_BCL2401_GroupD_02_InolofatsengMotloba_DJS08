6ce9d312d60025c580b101355524ffa23a960f05
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-06-09 23:56:23 +0200

    Create an initial Login Form

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import HostVanInfo from "./pages/Host/HostVanInfo";
 import HostVanPricing from "./pages/Host/HostVanPricing";
 import HostVanPhotos from "./pages/Host/HostVanPhotos";
 import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
 
 import "./server";
 
@@ -28,6 +29,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="login" element={<Login />} />
 
           <Route path="host" element={<HostLayout />}>
             <Route index element={<Dashboard />} />
