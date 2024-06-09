2901eb4ce7d2ddb597c9e52f51a8fc484e8c2a50
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-06-09 14:59:33 +0200

    Created a components folder and stored Header component in a separate file

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,7 @@ import "./server";
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">
-          #VANLIFE
-        </Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
