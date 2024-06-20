9f3e530f9760274b477e1e844b76e38c64f3a7dc
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-06-20 18:03:39 +0200

    Add comments to Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from "./Header";
-import Footer from "./Footer";
+import Header from "./Header"; // Importing the Header component
+import Footer from "./Footer"; // Importing the Footer component
 
+// Layout component to define the basic structure of the site layout
 export default function Layout() {
   return (
     <div className="site-wrapper">
+      {/* Header component */}
       <Header />
+
       <main>
+        {/* child routes/components */}
         <Outlet />
       </main>
+
+      {/* Footer component */}
       <Footer />
     </div>
   );
