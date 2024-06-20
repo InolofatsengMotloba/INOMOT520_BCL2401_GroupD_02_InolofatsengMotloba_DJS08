c792eeaddbef50f8db7f5c704b470c17eb99848d
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-06-20 17:56:27 +0200

    Add comments to Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,22 +2,29 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import imageUrl from "/src/assets/images/avatar-icon.png";
 
+// The Header component with navigation bar and logo
 export default function Header() {
+  // Styles to apply to active navigation links
   const activeStyles = {
     fontWeight: "bold",
     textDecoration: "underline",
     color: "#161616",
   };
 
+  // Function to simulate logging out by removing the 'loggedin' item from local storage.
   function fakeLogOut() {
     localStorage.removeItem("loggedin");
   }
 
   return (
+    // Render the header element.
     <header>
+      {/* Link to the home page with the site logo */}
       <Link className="site-logo" to="/">
         #VanLife
       </Link>
+
+      {/* Navigation bar */}
       <nav>
         <NavLink
           to="/host"
