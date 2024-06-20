133213c43465093d4c6ed5da7b22d5186df099fb
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-06-20 17:52:57 +0200

    Add comments to Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+// The Footer component renders a footer element with copyright information.
 export default function Footer() {
-  return <footer>&#169; 2022 #VANLIFE</footer>;
+  return (
+    // Render the footer with the copyright symbol and text
+    <footer>&#169; 2022 #VANLIFE</footer>
+  );
 }
